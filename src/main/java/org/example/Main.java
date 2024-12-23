ed8e3bd56bd7b7fda158ecab43c2bf32932c8882
ohnoesganj <sunho3456@naver.com>
2024-12-23 16:30:08 +0900

    7단계 요구사항

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -64,7 +64,7 @@ class App {
                 return;
             }
         }
-        System.out.println(id + "번 명언이 존재하지 않습니다.");
+        System.out.println(id + "번 명언은 존재하지 않습니다.");
     }
 }
 
