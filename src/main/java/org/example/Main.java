1af8c4dc9bb67118af198de8fb8ebc459eab017c
ohnoesganj <sunho3456@naver.com>
2024-12-23 15:19:31 +0900

    2번 요구사항

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,15 +1,29 @@
 package org.example;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         App app = new App();
         app.run();
     }
 }
 
 class App {
-    public void run() {
+    public void run() throws IOException {
         System.out.println("== 명언 앱 ==");
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        System.out.print("명령) ");
+        if (br.readLine().equals("등록")) {
+            System.out.print("명언: ");
+            br.readLine();
+            System.out.print("작가: ");
+            br.readLine();
+        }
+
         System.out.println("명령) 종료");
     }
 }
