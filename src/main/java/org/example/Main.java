b968413f1347f76a7e9c739863f69b3032a080ac
ohnoesganj <sunho3456@naver.com>
2024-12-23 15:30:28 +0900

    4번 요구사항

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -15,6 +15,7 @@ class App {
     public void run() throws IOException {
         System.out.println("== 명언 앱 ==");
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int cnt = 0;
 
         while (true) {
             System.out.print("명령) ");
@@ -24,10 +25,11 @@ class App {
                 br.readLine();
                 System.out.print("작가: ");
                 br.readLine();
-                System.out.println("1번 명언이 등록되었습니다.");
+                cnt++;
+                System.out.println(cnt + "번 명언이 등록되었습니다.");
+            } else {
+                break;
             }
-
-            System.out.println("명령) 종료");
         }
 
     }
